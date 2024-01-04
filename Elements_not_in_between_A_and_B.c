6e5268a279f801318d954467849fe1e2d6fc6be1
23p31a0560 <147791928+23p31a0560@users.noreply.github.com>
2024-01-04 20:03:39 +0530

    #include<stdio.h> int main() {     int b,c=0,d,i,j,k,e=0,x,y;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&x,&y);     for(j=0;j<b;j++)     {         if(a[j]>=x && a[j]<=y)         {             continue;         }             printf("%d ",a[j]);         e++;     }     if(e==0)     {         printf("-1");     } }

---
+++ Elements_not_in_between_A_and_B.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int b,c=0,d,i,j,k,e=0,x,y;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&x,&y);
+    for(j=0;j<b;j++)
+    {
+        if(a[j]>=x && a[j]<=y)
+        {
+            continue;
+        }
+            printf("%d ",a[j]);
+        e++;
+    }
+    if(e==0)
+    {
+        printf("-1");
+    }
+}
+    
