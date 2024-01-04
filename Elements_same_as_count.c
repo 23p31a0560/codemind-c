b2681923c66c38ea3e9bc77a2e69d958ae8feb61
23p31a0560 <147791928+23p31a0560@users.noreply.github.com>
2024-01-04 20:20:59 +0530

    #include<stdio.h> int main() {     int b,d,i,j,e=0;     scanf("%d",&b);     int a[b];     int c[b]={0};     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<b;i++)     {         c[a[i]]++;     }     for(i=1;i<b;i++)         {             if(c[i]==i)
           {
               j=i-1;
               printf("%d ",a[j]);
               e++;
           }
        }
        if(e==0)
        {
            printf("-1");
        }
    }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int b,d,i,j,e=0;
+    scanf("%d",&b);
+    int a[b];
+    int c[b]={0};
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<b;i++)
+    {
+        c[a[i]]++;
+    }
+    for(i=1;i<b;i++)
+        {
+            if(c[i]==i)
+            
+        
+       {
+           j=i-1;
+           printf("%d ",a[j]);
+           e++;
+       }
+    }
+    if(e==0)
+    {
+        printf("-1");
+    }
+}
+    
