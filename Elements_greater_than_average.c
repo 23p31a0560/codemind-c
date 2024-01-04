b020930f3016f3bad378112107ae2e724741af04
23p31a0560 <147791928+23p31a0560@users.noreply.github.com>
2024-01-04 19:47:44 +0530

    #include<stdio.h> int main() {     int b,c=0,d,i,j,k,e;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);        c=c+a[i];     }     d=c/(float)b;     for(j=0;j<b;j++)     {         if(a[j]>=d)         {             e++;         }     }     printf("%d",e); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int b,c=0,d,i,j,k,e;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+       c=c+a[i];
+    }
+    d=c/(float)b;
+    for(j=0;j<b;j++)
+    {
+        if(a[j]>=d)
+        {
+            e++;
+        }
+    }
+    printf("%d",e);
+}
+    
