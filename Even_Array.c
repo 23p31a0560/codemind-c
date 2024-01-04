71ad01990352ef517a890c51f6b8e1ca5a649f1f
23p31a0560 <147791928+23p31a0560@users.noreply.github.com>
2024-01-04 19:42:31 +0530

    #include<stdio.h> int main() {     int b,c=0,d,i,j,k,e;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);         if(a[i]%2==0)         {             c++;         }     }     if(c==b)     {         printf("True");     }     else     {         printf("False");     }  }

---
+++ Even_Array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int b,c=0,d,i,j,k,e;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2==0)
+        {
+            c++;
+        }
+    }   
+    if(c==b)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+ }
+    
